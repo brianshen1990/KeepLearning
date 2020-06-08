1186822ad83a27ffed0b6cf67228ed8d80adb09f
brianshen <shenhw3@asiainfo-sec.com>
2020-06-08 12:45:06 -0300

    UI autocomplete

--- h5_css3/autocomplete/index.js
+++ h5_css3/autocomplete/index.js
@@ -1,24 +1,25 @@
-
 const AutoCompletion = () => {
   const OPTIONS = [ "amazing", "astonished", "bravo", "cool", "done", "ebook", "fantastic", "great"];
 
   const helperClear = () => {
+    console.log("clear");
     const fillPlace = document.querySelector(".search-input-auto-complete");
     fillPlace.innerHTML = '';
   }
   const helperSelectOne =  (val) => {
     const input = document.querySelector(".search-input");
     input.value = val;
-    helperAuto(val);
+    helperShowRecommendations(val);
   }
-  const helperAuto = (val) => {
+
+  const helperShowRecommendations = (val) => {
     const fragments = document.createDocumentFragment(); // avoid manipulate dom frequently
     OPTIONS.filter( item => item.includes(val) ).filter( item => item !== val ).map( item => {
       const btn =  document.createElement("button");
       btn.className = "search-input-auto-complete-btn";
       btn.textContent = item;
       btn.onclick = () => {
-        console.log("clicked");
+        console.log(`clicked ${item}`);
         helperSelectOne(item);
       }
       fragments.appendChild(btn);
@@ -27,20 +28,19 @@ const AutoCompletion = () => {
     fillPlace.innerHTML = '';
     fillPlace.appendChild( fragments );
   }
-  let interval = null;
+  
   let value = "";
-
-  const helperStart = (force) => {
+  const helperHandleInterval = (force) => {
     const input = document.querySelector(".search-input");
     const val = ( input && input.value ) || "";
-    if ( force && val ) {
+    if ( force && val ) { // force refresh when re-enter
       value = val;
-      helperAuto(val);
+      helperShowRecommendations(val);
     } else {
       if ( value !== val ) { // only when value changes
         value = val;
         if ( val ) {
-          helperAuto(val);
+          helperShowRecommendations(val);
         } else {
           helperClear();
         }
@@ -48,20 +48,20 @@ const AutoCompletion = () => {
     }
   }
   
+  let interval = null;
   return {
-    start: () => {
+    start: () => { // start timer to monitor
       if ( !interval ) {
-        helperStart(true);
-        interval = setInterval( () => helperStart(),  200 )
+        helperHandleInterval(true);
+        interval = setInterval( () => helperHandleInterval(),  200 )
       }
     },
-    stop: () => {
-      console.log("stop");
+    stop: () => {  // stop timer and hide recommendations
       if ( interval ) {
         clearInterval(interval);
         interval = null;
       }
-      setTimeout( helperClear, 50 );
+      setTimeout(helperClear, 100);
     }
   }
 }
