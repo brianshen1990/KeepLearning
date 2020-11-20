1746c1d572532a04ae20f008b49071ceb38932b5
Shen, Brian <bshen@cvent.com>
2020-11-19 21:36:41 -0400

    Add leetcode 5 items

---
+++ leetcode/L1200/lc1309.js
@@ -0,0 +1,89 @@
+/**
+
+1309. Decrypt String from Alphabet to Integer Mapping
+
+Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively. 
+Return the string formed after mapping.
+
+It's guaranteed that a unique mapping will always exist.
+
+ 
+
+Example 1:
+
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+Example 2:
+
+Input: s = "1326#"
+Output: "acz"
+Example 3:
+
+Input: s = "25#"
+Output: "y"
+Example 4:
+
+Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+Output: "abcdefghijklmnopqrstuvwxyz"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] only contains digits letters ('0'-'9') and '#' letter.
+s will be valid string such that mapping is always possible.
+ */
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function(s) {
+    const aCharCode = 'a'.charCodeAt(0) - 1;
+    let arr = s.split("#").filter(item=>item).map( item => parseInt(item) );
+    // console.log(arr);
+    
+    return arr.map( (item, index) => {
+        if ( item < 10 ) {
+            return item;
+        } else {
+            if ( index < arr.length-1 ) {
+                let ret = [];
+
+                ret.push( item % 100 );
+                item = Math.floor( item / 100);
+
+                while( item > 0 ) {
+                    ret.push( item % 10 );
+                    item = Math.floor(item / 10);
+                }
+                return ret.reverse();
+            } else {
+                // last one
+                let ret = [];
+                if ( s[s.length-1] === "#" ) {
+                    ret.push( item % 100 );
+                    item = Math.floor( item / 100);
+                }
+                while( item > 0 ) {
+                    ret.push( item % 10 );
+                    item = Math.floor(item / 10);
+                }
+                return ret.reverse();
+            }
+        }
+    }).flat().map( item => String.fromCharCode(aCharCode+item) ).join("");
+    
+};
+
+/**
+"10#11#12"
+"1326#"
+"25#"
+"12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+ */
